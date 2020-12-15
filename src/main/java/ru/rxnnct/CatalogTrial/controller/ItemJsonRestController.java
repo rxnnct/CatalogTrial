3254485aa11d9965a2a1301ca64fd35ea708a11f
Rixen <rixennoct@rambler.ru>
2020-12-16 02:00:48 +0300

    Basic REST without filter.

---
+++ src/main/java/ru/rxnnct/CatalogTrial/controller/ItemJsonRestController.java
@@ -0,0 +1,50 @@
+package ru.rxnnct.CatalogTrial.controller;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.rxnnct.CatalogTrial.model.Item;
+import ru.rxnnct.CatalogTrial.repository.ItemRepo;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/json/items")
+public class ItemJsonRestController {
+    private final ItemRepo itemRepo;
+
+    @Autowired
+    public ItemJsonRestController(ItemRepo itemRepo) {
+        this.itemRepo = itemRepo;
+    }
+
+    @GetMapping
+    public List<Item> getAll() {
+        return itemRepo.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Item getOne(@PathVariable("id") Item item) {
+        return item;
+    }
+
+    @PostMapping
+    public Item create(@RequestBody Item item) {
+        return itemRepo.save(item);
+    }
+
+    @PutMapping("{id}")
+    public Item update(
+            @PathVariable("id") Item itemFromDb,
+            @RequestBody Item item
+    ) {
+        BeanUtils.copyProperties(item, itemFromDb, "id");
+        return itemRepo.save(itemFromDb);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") Item item) {
+        itemRepo.delete(item);
+    }
+
+}
--- src/main/java/ru/rxnnct/CatalogTrial/model/Item.java
+++ src/main/java/ru/rxnnct/CatalogTrial/model/Item.java
@@ -1,10 +1,8 @@
 package ru.rxnnct.CatalogTrial.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "items")
@@ -18,7 +16,4 @@ public class Item {
     private String name;
     @Column(name = "description")
     private String description;
-    @Column(name = "record_date_time", updatable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
-    private LocalDateTime creationDateTime;
 }
